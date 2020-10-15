6b0833c399536706084a6e3a226ccc507d3ab208
Mahendra Mavani <mahendra_mavani@dell.com>
2020-10-14 20:40:48 -0500

    Initial publish of luigi react example

---
+++ precache-manifest.29f03056ba7774729a26dfe1c802a4d9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5cdc6c569eca9d1eacd696d6addf4fc4",
+    "url": "/luigi-react/index.html"
+  },
+  {
+    "url": "/luigi-react/luigi-core/0072d92bfeaf57f02de04342d06a3463.woff2"
+  },
+  {
+    "url": "/luigi-react/luigi-core/1e678655fe64a8a126dd51619f22edde.woff"
+  },
+  {
+    "url": "/luigi-react/luigi-core/33bdbe44954b2ca9b5e7bbc0d5ab8860.woff"
+  },
+  {
+    "url": "/luigi-react/luigi-core/48cb3e07e7e509d6c81c420507d7916b.woff"
+  },
+  {
+    "url": "/luigi-react/luigi-core/5d817eee24266da78f96ef5e36c9019a.woff"
+  },
+  {
+    "url": "/luigi-react/luigi-core/85e09d643e5d4b5f98c3aaf365bd1c02.woff2"
+  },
+  {
+    "url": "/luigi-react/luigi-core/8b3b97ff8e0a414140609055fced4d8e.woff2"
+  },
+  {
+    "revision": "e1260bf3ad76be6ab1c30ac39aaffabf",
+    "url": "/luigi-react/luigi-core/README.md"
+  },
+  {
+    "url": "/luigi-react/luigi-core/f6226f10e41be591218339108cc242a1.woff2"
+  },
+  {
+    "revision": "07a83ef28dbcc5866b841094057e7150",
+    "url": "/luigi-react/luigi-core/luigi.css"
+  },
+  {
+    "revision": "ab48fa4759ad3bbe97fb7e43aa6a5e65",
+    "url": "/luigi-react/luigi-core/luigi.js"
+  },
+  {
+    "revision": "964cdbe5181f9d31ceb14499d71a969d",
+    "url": "/luigi-react/luigi-core/package.json"
+  },
+  {
+    "revision": "7e8e4e15d69c30f38bfc581f3530d30e",
+    "url": "/luigi-react/sampleapp.html"
+  },
+  {
+    "revision": "d00e9a85210461325cb0",
+    "url": "/luigi-react/static/css/2.e2a9e869.chunk.css"
+  },
+  {
+    "revision": "ce43822dd001a824d8eb",
+    "url": "/luigi-react/static/css/main.ea413d73.chunk.css"
+  },
+  {
+    "revision": "d00e9a85210461325cb0",
+    "url": "/luigi-react/static/js/2.0533f226.chunk.js"
+  },
+  {
+    "revision": "ce43822dd001a824d8eb",
+    "url": "/luigi-react/static/js/main.dfba8aa4.chunk.js"
+  },
+  {
+    "revision": "aaeb65f394f37af60bd7",
+    "url": "/luigi-react/static/js/runtime-main.eee60e35.js"
+  }
+]);
